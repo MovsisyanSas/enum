ae93b1091e6afaa2fc4da4ea2d0a8b9d4cc8e8f0
MovsisyanSas <sasunmov07@gmail.com>
2023-07-06 14:24:26 +0400

    changing convertors' style

--- enum.cpp
+++ enum.cpp
@@ -25,105 +25,153 @@ enum europe {
 };
 
 class cars {
-	void convert_j() {
-		for (int i = 0; i < 8; i++)
-		{
-			switch (i)
-			{
-			case toyota:
-				arr1[i] = "toyota";
-				break;
-			case nissan:
-				arr1[i] = "nissan";
-				break;
-			case mitsubishi:
-				arr1[i] = "mitsubishi";
-				break;
-			case subaru:
-				arr1[i] = "subaru";
-				break;
-			case mazda:
-				arr1[i] = "mazda";
-				break;
-			case suzuki:
-				arr1[i] = "suzuki";
-				break;
-			case isuzu:
-				arr1[i] = "isuzu";
-				break;
-			case datsun:
-				arr1[i] = "datsun";
-				break;
-			default:
-				std::cout << "error" << std::endl;
-				break;
-			}
-		}
-	}
-	void convert_e() {
-		for (int i = 0; i < 10; i++)
-		{
-			switch (i)
-			{
-			case mercedes:
-				arr2[i] = "mercedes";
-				break;
-			case bmw:
-				arr2[i] = "bmw";
-				break;
-			case volkswagen:
-				arr2[i] = "volkswagen";
-				break;
-			case audi:
-				arr2[i] = "audi";
-				break;
-			case porsche:
-				arr2[i] = "porsche";
-				break;
-			case opel:
-				arr2[i] = "opel";
-				break;
-			case lotus:
-				arr2[i] = "lotus";
-				break;
-			case mclaren:
-				arr2[i] = "mclaren";
-				break;
-			case alfaromeo:
-				arr2[i] = "alfaromeo";
-				break;
-			case astonmartin:
-				arr2[i] = "astonmartin";
-				break;
-			default:
-				std::cout << "error" << std::endl;
-				break;
-			}
-		}
-	}
-	std::string* arr1;
-	std::string* arr2;
 public:
 	cars() {
-		arr1 = new std::string[8];
-		arr2 = new std::string[10];
-		convert_e();
-		convert_j();
+		
 	}
-	std::string get_j(japan j) {
-		return arr1[j];
+	std::string convert_j_str(japan i) {
+
+		switch (i)
+		{
+		case toyota:
+			return "toyota";
+		case nissan:
+			return "nissan";
+		case mitsubishi:
+			return "mitsubishi";
+		case subaru:
+			return "subaru";
+		case mazda:
+			return "mazda";
+		case suzuki:
+			return "suzuki";
+		case isuzu:
+			return "isuzu";
+		case datsun:
+			return "datsun";
+		default:
+			return "error";
+		}
 	}
-	std::string get_e(europe e) {
-		return arr2[e];
+	std::string convert_e_str(europe i) {
+
+		switch (i)
+		{
+		case mercedes:
+			return "mercedes";
+		case bmw:
+			return "bmw";
+		case volkswagen:
+			return "volkswagen";
+		case audi:
+			return "audi";
+		case porsche:
+			return "porsche";
+		case opel:
+			return "opel";
+		case lotus:
+			return "lotus";
+		case mclaren:
+			return "mclaren";
+		case alfaromeo:
+			return "alfaromeo";
+		case astonmartin:
+			return "astonmartin";
+		default:
+			return "error";
+		}
 	}
+	int convert_e_enum(std::string s) {
+		if (s == "mercedes")
+		{
+			return mercedes;
+		}
+		else if (s == "bmw")
+		{
+			return bmw;
+		}
+		else if (s == "volkswagen")
+		{
+			return volkswagen;
+		}
+		else if (s == "audi")
+		{
+			return audi;
+		}
+		else if (s == "porsche")
+		{
+			return porsche;
+		}
+		else if (s == "opel")
+		{
+			return opel;
+		}
+		else if (s == "lotus")
+		{
+			return lotus;
+		}
+		else if (s == "mclaren")
+		{
+			return mclaren;
+		}
+		else if (s == "mclaren")
+		{
+			return mclaren;
+		}
+		else if (s == "mclaren")
+		{
+			return mclaren;
+		}
+		else {
+			return -1;
+		}
+	}
+	int convert_j_enum(std::string s) {
+		if (s == "toyota")
+		{
+			return toyota;
+		}
+		else if (s == "nissan")
+		{
+			return nissan;
+		}
+		else if (s == "mitsubishi")
+		{
+			return mitsubishi;
+		}
+		else if (s == "subaru")
+		{
+			return subaru;
+		}
+		else if (s == "mazda")
+		{
+			return mazda;
+		}
+		else if (s == "suzuki")
+		{
+			return suzuki;
+		}
+		else if (s == "alfaromeo")
+		{
+			return alfaromeo;
+		}
+		else if (s == "astonmartin")
+		{
+			return astonmartin;
+		}
+		else {
+			return -1;
+		}
+	}
+
 	~cars() {
-		delete[] arr1;
-		delete[] arr2;
 	}
 };
 
 int main() {
 	cars car;
-	std::cout << car.get_e(astonmartin) << std::endl;
-	std::cout << car.get_j(toyota) << std::endl;
+	std::cout << car.convert_e_str(lotus) << std::endl;
+	std::cout << car.convert_e_enum("lotus") << std::endl;
+	std::cout << car.convert_j_str(mazda) << std::endl;
+	std::cout << car.convert_j_enum("mazda") << std::endl;
 }
